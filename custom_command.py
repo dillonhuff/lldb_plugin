20cca949b21ab507dddf371ce5860ff20f146c69
Dillon Huff <dillonhuff@gmail.com>
2017-04-05 23:00:54 -0700

    Now have a little my_cmd command working

--- custom_command.py
+++ custom_command.py
@@ -3,10 +3,10 @@ import commands
 import optparse
 import shlex
 
-def ls(debugger, command, result, internal_dict):
-    print >>result, (commands.getoutput('/bin/ls %s' % command))
+def my_cmd(debugger, command, result, internal_dict):
+    print 'My command executed!'
     
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand('command script add -f custom_command.ls ls')
-    print 'The "ls" python command has been installed and is ready for use.'
+    debugger.HandleCommand('command script add -f custom_command.my_cmd my_cmd')
+    print 'The "my_cmd" python command has been installed and is ready for use.'
     
